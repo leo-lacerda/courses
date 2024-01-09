d44dc19ec60a626373579e0a67f55b0477b88c29
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-01-09 17:11:14 -0300

    Create social_media.py

---
+++ conditionals/social_media.py
@@ -0,0 +1,8 @@
+social_media_apps = ["Instagram", "TikTok", "Facebook", "X", "Twitter", "Threads", "YouTube", "Snapchat", "LinkedIn"]
+
+favorite_social_media = input("What's your favorite social media app? ")
+
+if favorite_social_media in social_media_apps:
+  print("That's one of the most famous!")
+else:
+  print("Are you sure that's a thing?")
