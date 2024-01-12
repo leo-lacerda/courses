b8dc2ae7971187b8589341de7b574ad96684bc2b
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-01-12 19:00:51 -0300

    Create comparing_numbers.py

---
+++ conditionals/comparing_numbers.py
@@ -0,0 +1,9 @@
+x = int(input("What's the first number? "))
+y = int(input("What's the second number? "))
+
+if x > y:
+  print("The first number is bigger than the second number.")
+elif x < y:
+  print("The first number is smaller than the second number.")
+else:
+  print("The first and second numbers are the same.")
