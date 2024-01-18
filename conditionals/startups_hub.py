0674882334eb7e108eccb51365ea94dec9b0d609
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-01-18 19:03:21 -0300

    Create startups_hub.py

---
+++ conditionals/startups_hub.py
@@ -0,0 +1,8 @@
+startups = ["Trilha Edu", "Be My Guest", "InfoMarket", "iDun", "Isanex", "Soffia"]
+
+startup = input("What's the startup name? ")
+
+if startup in startups:
+  print("That's a startup from Unifor Hub.")
+else:
+  print("I don't know this startup. Can you connect us?")
