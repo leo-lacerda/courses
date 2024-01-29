01d680c91c944536ab17aa0f012c1320c2b3372f
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-01-28 22:19:17 -0300

    Create startup.py

---
+++ functions/startup.py
@@ -0,0 +1,17 @@
+def main():
+  print(f"This is {get_name()}, {get_position()} at {get_startup()}.")
+        
+
+def get_name():
+  return input("What's your name? ")
+
+
+def get_position():
+  return input("What's your position? ")
+  
+  
+def get_startup():
+  return input("What's your startup name? ")
+  
+  
+main()
