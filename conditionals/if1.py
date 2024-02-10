0f5c150fae8ea53c5f5a10c48e153510a707f56f
Leo Lacerda <51065340+leo-lacerda@users.noreply.github.com>
2024-02-09 21:54:58 -0300

    Update if1.py

---
+++ conditionals/if1.py
@@ -0,0 +1,8 @@
+number = int(input("What's the number? ")
+
+if number > 7:
+    print("This number is bigger than 7")
+elif number < 7:
+    print("This number is smaller than 7")
+else:
+    print("This number is 7")
